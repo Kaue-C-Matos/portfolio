dcacb6ad8a9acfcf3d6024b2391de765d45e627b
Kaue-C-Matos <kauecostamatos31@gmail.com>
2026-03-03 11:44:24 -0300

    Criação do projeto, cabeçalho finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { Element, Link } from 'react-scroll';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Kauê da Costa Matos</h1>
+        <nav>
+          <Link to='sobre mim' smooth={true} offset={-120}>Sobre mim</Link>
+          <Link to='skills' smooth={true} offset={-120}>Skills</Link>
+          <Link to='hobbies' smooth={true} offset={-120}>Hobbies</Link>
+          <Link to='formacao' smooth={true} offset={-120}>Formação</Link>
+          <Link to='projetos' smooth={true} offset={-120}>Projetos</Link>
+        </nav>
       </header>
+      <main>
+        <Element name='sobre mim' id='sobremim'>
+          <h1>Sobre mim</h1>
+        </Element>
+
+        <Element name='skills'>
+          <h1>Skills</h1>
+        </Element>
+
+        <Element name='hobbies'>
+          <h1>Hobbies</h1>
+        </Element>
+
+        <Element name='formacao'>
+          <h1>Formação</h1>
+        </Element>
+
+        <Element id='projetos'>
+          <h1>Projetos</h1>
+        </Element>
+      </main>
     </div>
   );
 }
